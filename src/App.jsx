f9d529c72d2c368233a7746bee638e647a50e058
Ritesh Kumar Das <riteshhkumar6@gmail.com>
2025-10-21 10:36:40 +0530

    All file send

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import NavBar from './components/Navbar/NavBar'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Allproducts from './pages/Allproducts'
+import AboutMain from './pages/AboutMain'
+import Support from './pages/support'
+import { initNavbarScrollTrigger, cleanupScrollTrigger } from './script'
+
+function App() {
+  useEffect(() => {
+    // Initialize the navbar scroll trigger when component mounts
+    initNavbarScrollTrigger()
+    
+    // Cleanup function to remove ScrollTrigger instances when component unmounts
+    return () => {
+      cleanupScrollTrigger()
+    }
+  }, [])
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/all-products" element={<Allproducts />} />
+        <Route path="/about" element={<AboutMain />} />
+        <Route path="/support" element={<Support />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
